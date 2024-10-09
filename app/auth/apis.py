36c8aa371ed7ff4eea956e9dff7d25436d4d6dda
Bello Shehu Ango <angobello0@gmail.com>
2024-10-09 19:01:21 +0100

    improv: create tags

--- app/auth/apis.py
+++ app/auth/apis.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
 
 from app.auth.routes.oauth2 import router as oauth2_router
+from app.core.tags import get_tags
 
+# Globals
 router = APIRouter()
+tags = get_tags()
 
 # Include sub routes
-router.include_router(oauth2_router, prefix="/oauth2", tags=["Oauth2 Endpoints"])
+router.include_router(oauth2_router, prefix="/oauth2", tags=[tags.OAUTH2_ENDPOINT])
---
+++ app/core/tags.py
@@ -0,0 +1,24 @@
+from functools import lru_cache
+
+from pydantic import BaseModel
+
+
+class RouteTags(BaseModel):
+    """
+    Base model for app route tags
+    """
+
+    # Auth
+    AUTH: str = "Auth APIs"
+    OAUTH2_ENDPOINT: str = "Oauth2 Endpoints"
+
+    # User
+    USER: str = "User APIs"
+
+
+@lru_cache
+def get_tags():
+    """
+    Get app rotue tags
+    """
+    return RouteTags()
